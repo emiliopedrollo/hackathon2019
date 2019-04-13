8ff699fc38fa1391407cf743f6b677635bbae268
Emilio B. Pedrollo <emiliopedrollo@gmail.com>
2019-04-13 03:59:13 -0300

    Make test more covered

--- tests/Unit/HelperTest.php
+++ tests/Unit/HelperTest.php
@@ -10,7 +10,7 @@ class HelperTest extends TestCase
 {
 
     public function testItCanCalculateCPFValidatorDigit() {
-        $this->assertEquals('36269161894',cpf('362691618'));
+        $this->assertEquals('36269162009',cpf('362691620'));
         $this->assertEquals('472.352.473-80',cpf('472352473',true));
     }
 
