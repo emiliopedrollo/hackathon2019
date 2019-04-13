5c9ccb028bd20a2eba7d5fa9eb9c98fdeeec613e
Gilberto Russi <gilesrussi@gmail.com>
2019-04-12 22:24:32 -0300

    Notes seeder

--- database/seeds/NotesTableSeeder.php
+++ database/seeds/NotesTableSeeder.php
@@ -11,6 +11,15 @@ class NotesTableSeeder extends Seeder
      */
     public function run()
     {
-        factory()
+        factory(\App\Note::class)->create(5000)->each(function (\App\Note $note) {
+            $value = factory(\App\Product::class, [
+                'note_id' => $note->id,
+            ])->create(random_int(1, 50))->sum('value');
+
+            $note->update([
+                'total_value' => $value,
+                'discount_value' => $value * random_int(1, 10) / 100,
+            ]);
+        });
     }
 }
