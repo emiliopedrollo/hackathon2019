066ca754dd1a479e66b3ecd7e00b903a90ce6ce5
Gilberto Russi <gilesrussi@gmail.com>
2019-04-12 22:34:13 -0300

    Add user identification token

---
+++ database/migrations/2019_04_13_013306_add_user_identification_token.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserIdentificationToken extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('identification_token')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
